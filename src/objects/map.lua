805df0e604546951f7c6a65999ab454846870acf
Jim Nelson <jim@jimnelson.us>
2021-12-31 18:36:15 -0800

    Map now looks terrain-ish

--- src/objects/map.lua
+++ src/objects/map.lua
@@ -18,7 +18,16 @@ local function perlin_map_data()
     local map_image_data = love.image.newImageData(1600, 1200)
     map_image_data:mapPixel(function(x,y)
         local h = noise(vec2:new(x,y))
-        return h, h, h, 1
+        -- water
+        if h < 0.5 then
+            return 0, 0.412, 0.58, 1
+        end
+        -- grasslands
+        if h < 0.9 then
+            return 0.22, 0.58, 0.2, 1
+        end
+        -- plains
+        return 0.95, 0.86, 0.519, 1
     end)
     local image = love.graphics.newImage(map_image_data)
     return image
@@ -38,8 +47,8 @@ function Map:new()
     local map = {}
 
     function map:load()
-        -- map_image = perlin_map_data()
-        map_image = generated_map()
+        map_image = perlin_map_data()
+        -- map_image = generated_map()
     end
 
     function map:draw(pos)
