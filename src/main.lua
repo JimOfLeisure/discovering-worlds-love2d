a7ff6372c8d395cac6c59a9503ed1af52028311e
Jim Nelson <jim@jimnelson.us>
2021-12-31 16:19:16 -0800

    Think keyboard input working, need to adjust "map"

--- src/main.lua
+++ src/main.lua
@@ -10,9 +10,17 @@ local player = Player:new()
 
 function love.load()
 end
+
 function love.update(dt)
 end
+
 function love.draw()
     map:draw()
     player:draw()
+end
+
+function love.keypressed(key)
+    if love.keyboard.isDown("left") then
+        player.pos:move(vec2:new(-10, 0))
+    end
 end
