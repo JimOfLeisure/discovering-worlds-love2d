75a2e745f9d5dfaf2e519b9fd78a44f5217894ea
Jim Nelson <jim@jimnelson.us>
2021-12-31 18:12:56 -0800

    starting on map generation

---
+++ src/objects/continents.lua
@@ -0,0 +1,3 @@
+Continents = {}
+
+return Continents
--- src/objects/map.lua
+++ src/objects/map.lua
@@ -24,16 +24,27 @@ local function perlin_map_data()
     return image
 end
 
+local function generated_map()
+    local map_image_data = love.image.newImageData(1600, 1200)
+    map_image_data:mapPixel(function(x,y)
+        -- temporarily, everything is ocean blue
+        return 0, 0.412, 0.58, 1
+    end)
+    local image = love.graphics.newImage(map_image_data)
+    return image
+end
+
 function Map:new()
     local map = {}
-    map.seed = vec2:new(0.1, 0.1)
-    map.noise_mult = 0.002
-    function map:noise(pos)
-        return love_noise(pos.x * self.noise_mult + self.seed.x, pos.y * self.noise_mult + self.seed.y)
-    end
+    -- map.seed = vec2:new(0.1, 0.1)
+    -- map.noise_mult = 0.002
+    -- function map:noise(pos)
+    --     return love_noise(pos.x * self.noise_mult + self.seed.x, pos.y * self.noise_mult + self.seed.y)
+    -- end
 
     function map:load()
-        map_image = perlin_map_data()
+        -- map_image = perlin_map_data()
+        map_image = generated_map()
     end
 
     function map:draw(pos)
